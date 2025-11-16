06f992b410e85187fe2c38c7b10f88bebcfb05fa
Bidisha Das <biplobchandradas1974@gmail.com>
2025-11-16 22:27:17 +0530

    Initial commit

---
+++ assignment1a.py
@@ -0,0 +1,39 @@
+#Q.1
+name=input("enter your name: ")
+age=int(input("enter your age: "))
+print("Hello ",name,","," you are",age,"years old!")
+
+#Q.2
+num1=int(input("enter first number: "))
+num2=int(input("enter second number: "))
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)
+print(num1//num2)
+print(num1**num2)
+
+
+#Q.3
+a=float(input("enter number 1:"))
+b=float(input("enter number 2:"))
+c=float(input("enter number 3: "))
+avg=(a+b+c)/3
+print("the average of three numbers is:",avg)
+#Q.4
+
+
+number=input("enter number: ")
+
+a=int(number)
+
+print(a,type(a))
+b=float(number)
+print(b,type(b))
+c=str(number)
+print(c,type(c))
+
+#Q.5
+
+x=10+3*2**2
+print(x)
