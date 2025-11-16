2ccbd026db747d27a9ddd7631ea3e1079bd04f6e
Bidisha Das <biplobchandradas1974@gmail.com>
2025-11-16 22:28:05 +0530

    Initial commit

---
+++ assignment1b.py
@@ -0,0 +1,35 @@
+#Q.6
+
+# a=5
+# b=2
+# a,b = b,a
+# print("a =",a)
+# print("b =",b)
+
+# #Q.7
+# temp=float(input("enter temperature in celsius: "))
+# fahrenheit=(temp*9/5)+32
+# print("temperature in fahrenheit is: ",fahrenheit)
+
+
+# #Q.8
+# r=float(input("enter radius of circle: "))
+# pi=3.14
+# area=pi*r**2
+# print("area of circle is: ",area)
+
+
+#Q.9
+# p=float(input("Enter the principal amount: "))
+
+# r=float(input("Enter the rate of interest: "))
+# t=float(input("Enter the time in years: "))
+# sI=(p*r*t)/100
+# print("Simple Interest is: ",sI)
+
+#Q.10
+dec_num=float(input("enter a decimal number: "))
+print("Integer part: ",int(dec_num))
+print("Fractional part: ",(dec_num-int(dec_num)))
+
+
