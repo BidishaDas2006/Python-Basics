71605c574ec3c6cc917618772b172c245fd9ecc1
Bidisha Das <biplobchandradas1974@gmail.com>
2025-11-20 18:12:43 +0530

    Initial Commit
    Uncommented code for questions 6 to 10 in assignment2b.py.

--- assignment2b.py
+++ assignment2b.py
@@ -1,56 +1,56 @@
 #Q.6
-# for i in range(1,101):
-#     if (i%3==0 and i%5==0):
-#         print(i)
+for i in range(1,101):
+    if (i%3==0 and i%5==0):
+        print(i)
 
 
 
-# #Q.7
+#Q.7
 
-# while True:
-#     n=input("enter a number: ")
+while True:
+    n=input("enter a number: ")
 
-#     if n=="Quit":
-#         print("you Quitted")
-#         break
-#     num=float(n)
-#     if num>0:
-#         print("positive")
+    if n=="Quit":
+        print("you Quitted")
+        break
+    num=float(n)
+    if num>0:
+        print("positive")
         
-#     else:
-#         print("negative") 
+    else:
+        print("negative") 
 
 #Q.8
 
  
-# def calculator(a, b, operation):
-#     match operation:
-#         case '+':
-#             return a + b
-#         case '-':
-#             return a - b
-#         case '*':
-#             return a * b
-#         case '/':
-#             return a / b
+def calculator(a, b, operation):
+    match operation:
+        case '+':
+            return a + b
+        case '-':
+            return a - b
+        case '*':
+            return a * b
+        case '/':
+            return a / b
 
-# print(calculator(20, 5, '*'))
+print(calculator(20, 5, '*'))
 
 
 #Q.9
 
-# def is_prime(n):
+def is_prime(n):
     
      
-#     for i in range(2,n-1):
-#           if n%i==0:
-#                print("False")
-#                break
-#     else:
-#         print("True")
+    for i in range(2,n-1):
+          if n%i==0:
+               print("False")
+               break
+    else:
+        print("True")
                
     
-# is_prime(7)
+is_prime(7)
 
 
 #Q.10
